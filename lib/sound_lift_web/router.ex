8a5885a184136961741bb59351204aff79a4a507
ShwnCndn <115516008+ShwnCndn@users.noreply.github.com>
2023-08-21 18:34:46 -0400

    user auth gen and username migration

--- lib/sound_lift_web/router.ex
+++ lib/sound_lift_web/router.ex
@@ -1,6 +1,8 @@
 defmodule SoundLiftWeb.Router do
   use SoundLiftWeb, :router
 
+  import SoundLiftWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule SoundLiftWeb.Router do
     plug :put_root_layout, html: {SoundLiftWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_user
   end
 
   pipeline :api do
@@ -20,7 +23,6 @@ defmodule SoundLiftWeb.Router do
     live "/audiometry", AudiometryLive
     get "/", PageController, :home
   end
-
 
   # Other scopes may use custom stacks.
   # scope "/api", SoundLiftWeb do
@@ -41,6 +43,44 @@ defmodule SoundLiftWeb.Router do
 
       live_dashboard "/dashboard", metrics: SoundLiftWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
+    end
+  end
+
+  ## Authentication routes
+
+  scope "/", SoundLiftWeb do
+    pipe_through [:browser, :redirect_if_user_is_authenticated]
+
+    live_session :redirect_if_user_is_authenticated,
+      on_mount: [{SoundLiftWeb.UserAuth, :redirect_if_user_is_authenticated}] do
+      live "/users/register", UserRegistrationLive, :new
+      live "/users/log_in", UserLoginLive, :new
+      live "/users/reset_password", UserForgotPasswordLive, :new
+      live "/users/reset_password/:token", UserResetPasswordLive, :edit
+    end
+
+    post "/users/log_in", UserSessionController, :create
+  end
+
+  scope "/", SoundLiftWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{SoundLiftWeb.UserAuth, :ensure_authenticated}] do
+      live "/users/settings", UserSettingsLive, :edit
+      live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
+    end
+  end
+
+  scope "/", SoundLiftWeb do
+    pipe_through [:browser]
+
+    delete "/users/log_out", UserSessionController, :delete
+
+    live_session :current_user,
+      on_mount: [{SoundLiftWeb.UserAuth, :mount_current_user}] do
+      live "/users/confirm/:token", UserConfirmationLive, :edit
+      live "/users/confirm", UserConfirmationInstructionsLive, :new
     end
   end
 end
--- priv/repo/migrations/20230821215027_add_username_to_user.exs
+++ priv/repo/migrations/20230821215027_add_username_to_user.exs
@@ -2,5 +2,8 @@ defmodule SoundLift.Repo.Migrations.AddUsernameToUser do
   use Ecto.Migration
 
   def change do
+    alter table(:users)
+      add :username, :string, null: false
+    end
   end
 end
