e42ddf952bec3ecc9948aa342fdd443e903eb06e
ShwnCndn <115516008+ShwnCndn@users.noreply.github.com>
2023-09-01 16:16:25 -0400

    index_live.ex compiles

--- lib/sound_lift_web/live/results/index_live.ex
+++ lib/sound_lift_web/live/results/index_live.ex
@@ -1,3 +1,15 @@
 defmodule SoundLiftWeb.Results.IndexLive do
+  use SoundLiftWeb, :live_view
+
+  def mount(_params, _session, socket) do
+    {:ok, socket}
+  end
+
+  def render(assigns) do
+~H"""
+
+"""
+  end
+
   
 end
